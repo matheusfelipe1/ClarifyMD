7b8157972f8f7b70dd6851651221dae316cbcfea
Matheus Felipe Silva Santos <68447982+matheusfelipe1@users.noreply.github.com>
2025-11-29 12:26:21 -0300

    first commit

---
+++ ClarifyMD/ClarifyMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClarifyMDApp.swift
+//  ClarifyMD
+//
+//  Created by Matheus Santos  on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ClarifyMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
